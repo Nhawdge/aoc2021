37d8d731b7faa7d95fdea1d2ef9860e027a79616
John <john.pavek@blendinteractive.com>
2021-12-06 22:43:01 -0600

    Day 3 was miserable

--- DaysCode/Day3.cs
+++ DaysCode/Day3.cs
@@ -26,24 +26,47 @@ public static class Day3
     public static void SolvePart2()
     {
         var allLines = File.ReadAllLines("Data/day3.txt");
-        var matrix = allLines.Select(x => x.Split("").ToList());
         var firstLine = allLines[0];
         var index = 0;
-        var mostCommon = 0 << 0;
-
-        foreach (var character in firstLine.Split(""))
+        var validO2Values = allLines.ToList();
+        var validCO2Values = allLines.ToList();
+        foreach (var character in firstLine.ToArray())
         {
-            var commonBits = new List<string>();
-            foreach (var line in matrix)
+            //Console.WriteLine("Loop start ---------------");
+            if (validO2Values.Count() > 1)
             {
-                commonBits.Add(line[index]);
-            }
+                var mostCommon = "1";
 
-            mostCommon += commonBits.Count(x => x == "1") > commonBits.Count(x => x == "0") ? 1 << index : 0;
-            var leastCommon = ~mostCommon;
+                var mostCommonList = validO2Values.Select(x => x.Substring(index, 1)).GroupBy(x => x).OrderByDescending(x => x.Count());
+                if (mostCommonList.Count() == 1 || mostCommonList.First().Count() != mostCommonList.Last().Count())
+                {
+                    mostCommon = mostCommonList.First().Key;
+                }
+
+                //Console.WriteLine($"{mostCommonList.First().Key}: {mostCommonList.First().Count()} {mostCommonList.Last().Count()}");
+                //Console.WriteLine($"Filter: {index}, {mostCommon} Options:\n{string.Join("\n", validO2Values)}");
+                validO2Values = validO2Values.Where(x => x.Substring(index, 1) == mostCommon).ToList();
+            }
+            if (validCO2Values.Count() > 1)
+            {
+                var leastCommon = "0";
+                var leastCommonList = validCO2Values.Select(x => x.Substring(index, 1)).GroupBy(x => x).OrderBy(x => x.Count());
+                if (leastCommonList.Count() > 1 && leastCommonList.First().Count() != leastCommonList.Last().Count())
+                {
+                    leastCommon = leastCommonList.First().Key;
+                }
+                validCO2Values = validCO2Values.Where(x => x.Substring(index, 1) == leastCommon).ToList();
+            }
+            //Console.WriteLine($"Counts:\n  O2: {validO2Values.Count}\n  CO2: {validCO2Values.Count}");
             index++;
-            Console.WriteLine($"{Convert.ToString(mostCommon, 2)} {leastCommon}");
+
         }
 
+        var O2Level = Convert.ToInt32(validO2Values.FirstOrDefault(), 2);
+        var CO2Level = Convert.ToInt32(validCO2Values.First(), 2);
+        Console.WriteLine("------------------");
+        Console.WriteLine($"O2 Raw: {validO2Values.First().ToString()}\nCO2 Raw: {validCO2Values.First().ToString()}");
+        Console.WriteLine($"O2: {O2Level}\nCO2: {CO2Level}\nAnswer: {O2Level * CO2Level}");
     }
+
 }
